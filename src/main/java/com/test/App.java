28677fea8170870e6f111602cbdd2a5960780966
bhavsarjayant <bhavsarjayant.21@gmail.com>
2018-08-15 23:09:59 +0530

    App.java is modified for demo

--- src/main/java/com/test/App.java
+++ src/main/java/com/test/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println("Master branch!" );
+        System.out.println("Master branch! - Updated for demo" );
     }
 }
